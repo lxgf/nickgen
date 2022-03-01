87c2a29435e050d63aff04de9bcb495f821f94d6
lxgf <junerkukas@ya.ru>
2022-03-02 00:55:34 +0600

    console nickgen

---
+++ back/nickgen.py
@@ -0,0 +1,60 @@
+import random
+
+consonants = [
+    'b', 'c', 'd', 'f', 'g', 'h', 'j',
+    'k', 'l', 'm', 'n', 'p', 'q', 'r',
+    's', 't', 'v', 'w', 'x', 'y', 'z'
+]
+
+vowels = [
+    'a', 'e', 'i', 'o', 'u'
+]
+
+
+def get_consonant():
+    letter = consonants[random.randint(0, len(consonants) - 1)]
+    return letter
+
+
+def get_vowel():
+    letter = vowels[random.randint(0, len(vowels) - 1)]
+    return letter
+
+
+def generate_syllable(consonants_number: int, vowels_number: int):
+    match str(consonants_number) + str(vowels_number):
+        case '11':
+            return get_consonant() + get_vowel()
+        case '12':
+            return get_consonant() + get_vowel() + get_vowel()
+        case '21':
+            return get_consonant() + get_consonant() + get_vowel()
+        case _:
+            return get_consonant() + get_vowel()
+
+def generate_word(max_syllables_number: int, letters_in_syllable: int, pascal_case: bool):
+    if 2 <= max_syllables_number <= 10:
+        word = ''
+        for i in range(max_syllables_number):
+            if letters_in_syllable == 3:
+                if pascal_case:
+                    syllable = generate_syllable(random.randint(1, 2), random.randint(1, 2))
+                    word += syllable.title()
+                else:
+                    word += generate_syllable(random.randint(1, 2), random.randint(1, 2))
+            elif letters_in_syllable == 2:
+                if pascal_case:
+                    syllable = generate_syllable(1, 1)
+                    word += syllable.title()
+                else:
+                    word += generate_syllable(1, 1)
+            else:
+                print('Incorrect letters in syllable number (2 or 3)')
+                break
+
+        return word
+    else:
+        print('Incorrect syllables number (must be >=2 and <=10)')
+
+
+print(generate_word(4, 3, True))
